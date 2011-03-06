bea93246657e0a857af450fa9484d238a84df6d8
Rob Bayliss <rob@rbayliss.net>
2011-03-06 15:31:34 -0500

    Adding the jquery plugin

---
+++ ddtf.js
@@ -0,0 +1,122 @@
+(function($) {
+
+$(document).ready(function() {
+  var s = $('table').ddTableFilter();
+});
+
+$.fn.ddTableFilter = function(options) {
+  options = $.extend($.fn.ddTableFilter.defaultOptions, options);
+  
+  return this.each(function() {
+    if($(this).hasClass('ddtf-processed')) {
+      refreshFilters(this);
+      return;
+    }
+    var table = $(this);
+    var start = new Date();
+    
+    $('th:visible', table).each(function(index) {
+      if($(this).hasClass('skip-filter')) return;
+      var selectbox = $('<select>');
+      var values = [];
+      var opts = new Array();
+      selectbox.append('<option value="--all--">' + $(this).text() + '</option>');
+      
+      var col = $('td:nth-child(' + (index + 1) + ')').each(function(index) {
+        var cellVal = options.valueCallback.apply(this);
+        if(cellVal.length == 0) {
+          cellVal = '--empty--';
+        }
+        $(this).attr('ddtf-value', cellVal);
+        
+        if($.inArray(cellVal, values) === -1) {
+          var cellText = options.textCallback.apply(this);
+          if(cellText.length == 0) { cellText = options.emptyText; }
+          values.push(cellVal);
+          opts.push({val:cellVal, text:cellText});
+        }
+      });
+      if(opts.length < options.minOptions){
+        return;
+      } 
+      if(options.sortOpt) {
+        opts.sort(options.sortOptCallback);
+      }
+      $.each(opts, function() {
+        $(selectbox).append('<option value="' + this.val + '">' + this.text + '</option>')
+      });
+
+      $(this).wrapInner('<div style="display:none">');
+      $(this).append(selectbox);
+      
+      selectbox.bind('change', {column:col}, function(event) {
+        var changeStart = new Date();
+        var value = $(this).val();
+        
+        event.data.column.each(function() {
+          if($(this).attr('ddtf-value') === value || value == '--all--') {
+            $(this).removeClass('ddtf-filtered');
+          }
+          else {
+            $(this).addClass('ddtf-filtered');
+          }
+        });
+        var changeStop = new Date();
+        if(options.debug) {
+          console.log('Search: ' + (changeStop.getTime() - changeStart.getTime()) + 'ms');
+        }
+        refreshFilters(table);
+        var changeStop = new Date();
+        
+      });
+      table.addClass('ddtf-processed');
+    });
+    
+    
+    function refreshFilters(table) {
+      var refreshStart = new Date();
+      $('tr', table).each(function() {
+        var row = $(this);
+        if($('td.ddtf-filtered', row).length > 0) {
+          options.transition.hide.apply(row, options.transition.options);
+        }
+        else {
+          options.transition.show.apply(row, options.transition.options);
+        }
+      });
+      var refreshEnd = new Date();
+      if(options.debug) {
+        console.log('Refresh: ' + (refreshEnd.getTime() - refreshStart.getTime()) + 'ms');
+      }
+    }
+    
+    var stop = new Date();
+    if(options.debug) {
+      console.log('Build: ' + (stop.getTime() - start.getTime()) + 'ms');
+    }
+  });
+  
+}
+
+$.fn.ddTableFilter.defaultOptions = {
+  valueCallback:function() {
+    return escape($.trim($(this).text()));
+  },
+  textCallback:function() {
+    return $.trim($(this).text());
+  },
+  sortOptCallback: function(a, b) {
+    return a.text.toLowerCase() > b.text.toLowerCase(); 
+  },
+  transition: {
+    hide:$.fn.hide,
+    show:$.fn.show,
+    options: []
+  },
+  emptyText:'--Empty--',
+  sortOpt:true,
+  debug:false,
+  minOptions:2
+}
+ 
+})(jQuery);
